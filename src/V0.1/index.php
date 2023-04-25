03eb79e126deda6764737235a813829993dd8732
Cmoyano91 <cmoyano18@gmail.com>
2023-04-25 11:07:34 +0200

    mejora1

---
+++ src/V0.1/index.php
@@ -0,0 +1,31 @@
+<?php
+	require('funciones.inc');
+	
+	// --- Miramos si enviamos hoja para mostrar ---
+	$_SHEET = 'main';
+	
+	if ( !empty( $_SERVER['QUERY_STRING'] ) )
+	{
+		$_SHEET = $_SERVER['QUERY_STRING'];
+	}
+	
+	// --- Headers ---
+	include( 'headers.inc' );
+	
+	echo '<body onload="inicio();">';
+	
+	if ( file_exists( 'sheets/head.inc' ) )
+	{
+		include( 'sheets/head.inc' );
+	}
+	
+	include( 'sheets/' . $_SHEET . '.inc' );
+	
+	if ( file_exists( 'sheets/foot.inc' ) )
+	{
+		include( 'sheets/foot.inc' );
+	}
+	
+	echo '</body>';
+	
+	
